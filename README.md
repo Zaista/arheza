ae6aa6027ae8f07213087ca7ed999f4948613201
Jovan <jovan.ilic@cloudflight.io>
2023-11-07 00:17:11 +0100

    feat: add image generation

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ openai-env\Scripts\activate
 
 pip install --upgrade openai
 
+functions-framework-python --target hello_http
+
 gcloud functions deploy python-http-function --gen2 --runtime=python311 --region=europe-west3 --source=. --entry-point=hello_http --trigger-http --allow-unauthenticated
--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-from flask import escape
-from dotenv import dotenv_values
-from google.cloud import secretmanager
-
-import openai
+# from dotenv import dotenv_values
 import functions_framework
+from openai import OpenAI
+from google.cloud import secretmanager
 
 # env = dotenv_values(".env")
 # openai.api_key = env["OPENAI_API_KEY"]
-
 
 client = secretmanager.SecretManagerServiceClient()
 secret_name = "projects/307352298506/secrets/openai-api-key/versions/1"
 
 response = client.access_secret_version(name=secret_name)
-openai.api_key = response.payload.data.decode("UTF-8")
+# openai.api_key = response.payload.data.decode("UTF-8")
+client = OpenAI(api_key=response.payload.data.decode("UTF-8"))
 
 
 @functions_framework.http
@@ -27,23 +25,27 @@ def hello_http(request):
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
-    request_json = request.get_json(silent=True)
-    request_args = request.args
 
-    if request_json and "name" in request_json:
-        name = request_json["name"]
-    elif request_args and "name" in request_args:
-        name = request_args["name"]
+    if request.path == "/image":
+        print("requesting image")
+        image = client.images.generate(
+            prompt=request.get_json()["prompt"],
+            size="1024x1024",
+            n=1,
+        )
+
+        image_url = image.data[0].url
+        return image_url
     else:
-        name = "John Doe"
+        print("requesting chatGTP")
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a fantasy world tale teller, skilled in crafting mysterious and "
+                                              "engaging adventures."},
+                {"role": "user", "content": "Describe in up to 3 sentences a starting point for a hero going on a "
+                                            "first quest."}
+            ]
+        )
 
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming "
-                                          "concepts with creative flair. Start with a greeting to " + escape(name)},
-            {"role": "user", "content": "Compose a poem that explains the basic concept of chatGPT."}
-        ]
-    )
-
-    return completion.choices[0].message.content
+        return completion.choices[0].message.content
