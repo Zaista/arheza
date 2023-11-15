585215cdcb869e3e436dad9a0e84119c330b197d
Zaista <ilicjovan89@gmail.com>
2023-11-15 21:57:14 +0100

    feat: add flask

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-python -m venv openai-env
+python -m venv venv
 
-openai-env\Scripts\activate
+venv\Scripts\activate
 
 pip install --upgrade openai
-
-functions-framework-python --target hello_http
-
-gcloud functions deploy python-http-function --gen2 --runtime=python311 --region=europe-west3 --source=. --entry-point=hello_http --trigger-http --allow-unauthenticated
--- main.py
+++ main.py
@@ -1,51 +1,57 @@
-# from dotenv import dotenv_values
-import functions_framework
+import os
+from dotenv import dotenv_values
+from flask import Flask, request, render_template
 from openai import OpenAI
 from google.cloud import secretmanager
 
-# env = dotenv_values(".env")
-# openai.api_key = env["OPENAI_API_KEY"]
+if os.getenv('GAE_ENV', '').startswith('standard'):
+    client = secretmanager.SecretManagerServiceClient()
+    secret_name = "projects/307352298506/secrets/openai-api-key/versions/1"
+    secret = client.access_secret_version(name=secret_name)
+    apiKey = secret.payload.data.decode("UTF-8")
 
-client = secretmanager.SecretManagerServiceClient()
-secret_name = "projects/307352298506/secrets/openai-api-key/versions/1"
+else:
+    env = dotenv_values(".env")
+    apiKey = env["OPENAI_API_KEY"]
 
-response = client.access_secret_version(name=secret_name)
-# openai.api_key = response.payload.data.decode("UTF-8")
-client = OpenAI(api_key=response.payload.data.decode("UTF-8"))
+client = OpenAI(api_key=apiKey)
+
+app = Flask(__name__)
 
 
-@functions_framework.http
-def hello_http(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-    """
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    if request.path == "/image":
-        print("requesting image")
-        image = client.images.generate(
-            prompt=request.get_json()["prompt"],
-            size="1024x1024",
-            n=1,
-        )
 
-        image_url = image.data[0].url
-        return image_url
-    else:
-        print("requesting chatGTP")
-        completion = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are a fantasy world tale teller, skilled in crafting mysterious and "
-                                              "engaging adventures."},
-                {"role": "user", "content": "Describe in up to 3 sentences a starting point for a hero going on a "
-                                            "first quest."}
-            ]
-        )
+@app.route('/story', methods=['POST'])
+def get_story():
+    print("requesting story")
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a fantasy world tale teller, skilled in crafting mysterious and "
+                                          "engaging adventures."},
+            {"role": "user", "content": "Describe in up to 3 sentences a starting point for a hero going on a "
+                                        "first quest."}
+        ]
+    )
 
-        return completion.choices[0].message.content
+    return completion.choices[0].message.content
+
+
+@app.route('/image', methods=['POST'])
+def get_image():
+    print("requesting image")
+    image = client.images.generate(
+        prompt=request.get_json()["prompt"],
+        size="1024x1024",
+        n=1,
+    )
+
+    image_url = image.data[0].url
+    return image_url
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
